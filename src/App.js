829265822ed0b813731761aa79fd0c92e76f03cf
Vitor Juanh <vjuanhfranco@gmail.com>
2020-03-27 14:36:41 -0400

    connecting to heroku server

--- src/App.js
+++ src/App.js
@@ -171,7 +171,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imageURL: this.state.input})
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://cryptic-tor-14827.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -181,7 +181,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/rank', {
+        fetch('https://cryptic-tor-14827.herokuapp.com/rank', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -23,7 +23,7 @@ class SignUp extends React.Component {
     }
 
     onSubmitSignUp = () => {
-        fetch('http://localhost:3000/signup', {
+        fetch('https://cryptic-tor-14827.herokuapp.com/signup', {
             method: 'post',
             headers : {'Content-Type': 'application/json'},
             body: JSON.stringify({
